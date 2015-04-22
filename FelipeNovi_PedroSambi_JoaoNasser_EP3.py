ba54dbad13fe4d9771e75b19a17081e024f241d3
felipenovi <felipe.novi@terra.com.br>
2015-04-22 09:39:47 -0300

    Commit-5

--- FelipeNovi_PedroSambi_JoaoNasser_EP3.py
+++ FelipeNovi_PedroSambi_JoaoNasser_EP3.py
@@ -7,32 +7,32 @@ entrada_usuario = open("usuario.csv","r")
 entrada_usuario.readline()
 entrada_informacoes = entrada_usuario.readline()
 entrada_informacoes.strip()
-informacoes = entrada_informacoes.split(',')
+info = entrada_informacoes.split(',')
 
 
-nome = informacoes[0]
-sexo = informacoes[3]
-idade = int(informacoes[1])
-altura = float(informacoes[4])
-peso = float(informacoes[2])
-atividade = informacoes[5]
+nome = info[0]
+sexo = info[3]
+idade = int(info[1])
+altura = float(info[4])
+peso = float(info[2])
+atividade = info[5]
 
 
 IMC = peso/(altura**2)
 if IMC < 17:
-	imc_usuario = "muito abaixo do peso"
+	imc_usuario = "Você está muito abaixo do peso"
 elif IMC > 17 and IMC < 18.49:
-	imc_usuario = "abaixo do peso"
+	imc_usuario = "Você está abaixo do peso"
 elif IMC >=18.5 and IMC < 24.99:
-	imc_usuario = " no seu peso normal"
+	imc_usuario = "Você está no seu peso normal"
 elif IMC >25 and IMC <29.99:
-	imc_usuario = "acima do peso"
+	imc_usuario = "Você está acima do peso"
 elif IMC >30 and IMC< 34.99:
-	imc_usuario = "no nivel de obesidade 1"
+	imc_usuario = "Você está no nivel de obesidade 1"
 elif IMC >35 and IMC <39.99:
-	imc_usuario = "no nivel de obesidade 2"
+	imc_usuario = "Você está no nivel de obesidade 2"
 elif IMC>40:
-	imc_usuario = "no nível de obesidade morbida"
+	imc_usuario = "Você está no nível de obesidade morbida"
 print(imc_usuario)
 
 
@@ -56,4 +56,25 @@ entrada_usuario.readline()
 linhasdiario = entrada_usuario.readlines() 
 for line in linhasdiario:
     print(line)
+    
+    
+entrada_alimentos = open("alimentos.csv","r")
+linhas = entrada_alimentos.readlines()
+linhas_limpas = []                            
+for i in linhas:
+    linhas_limpas.append(i.strip())
+alimentos = {}
+for l in linhas_limpas[1:]:                        
+    info2 = l.split(',')
+     
+Alimento = info2[0] 
+Quantidade = float(info2[1])
+Calorias = float(info2[2]) 
+Proteínas = float(info2[3])
+Carboidratos = float(info2[4]) 
+Gorduras = float(info2[5])
+ 
+  
+
+    
     
