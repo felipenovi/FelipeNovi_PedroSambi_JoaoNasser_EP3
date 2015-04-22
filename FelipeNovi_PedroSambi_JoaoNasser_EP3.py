0122eb85fcd3364e5fbadb8f7f17f727ffa2403a
felipenovi <felipe.novi@terra.com.br>
2015-04-22 20:10:37 -0300

    Commit-8

--- FelipeNovi_PedroSambi_JoaoNasser_EP3.py
+++ FelipeNovi_PedroSambi_JoaoNasser_EP3.py
@@ -13,7 +13,7 @@ info = entrada_informacoes.split(',')
 
 nome = info[0]
 sexo = info[3]
-idade = int(info[1])
+idade = float(info[1])
 altura = float(info[4])
 peso = float(info[2])
 atividade = info[5]
@@ -42,14 +42,22 @@ arquivo_txt.write("O seu Indice de massa corporea é %s \n"%IMC)
 arquivo_txt.write("voce esta %s \n"%imc_usuario)
 
 
-"""
-formula consumo de caloria ideal
+if atividade == ("minimo"):
+    atividade = 1.2
+elif atividade == ("baixo"):
+    atividade = 1.375
+elif atividade == ("medio"):
+    atividade = 1.55
+elif atividade == ("alto"):
+    atividade = 1.725
+elif atividade == ("muito alto"):
+    atividade = 1.9
+#Fórmula consumo de caloria ideal
 if sexo == "m" or sexo =="M":
-	HBh = (88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade)) * atividade
-	
+	HB = (88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade)) 
 elif sexo == "f" or sexo == "F":
-	HBm = (447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * idade)) * atividade	
-"""	
+	HB = (447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * idade)) 	
+print(HB)
 
 
 diario = {}
@@ -61,8 +69,9 @@ for i in linhasdiario:
 for i in range(len(a)):
     a[i] = a[i].split(',')
     a[i][2] = float(a[i][2])
-
 print(a)
+
+
 alimento = {}   
 entrada_alimentos = open("alimentos.csv","r+")
 linhas = entrada_alimentos.readlines()
